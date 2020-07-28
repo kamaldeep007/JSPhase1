7c164ff138677eff7570bb46954eae5c8b9b254e
kamaldeep007 <68885616+kamaldeep007@users.noreply.github.com>
2020-07-27 23:27:24 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,56 @@
+var input = document.querySelector("#input");
+
+var addTask = document.querySelector("#addTask");
+var todo = document.querySelector("#todo");
+var completed = document.querySelector("#completed");
+var sound = document.querySelector("#playMusic");
+
+addTask.onclick = function () {
+  var userInput = input.value;
+  var div = document.createElement("div");
+  var p = document.createElement("p");
+  var btn = document.createElement("btn");
+
+  btn.textContent = "Done";
+  p.textContent = userInput;
+
+  div.appendChild(p);
+  div.appendChild(btn);
+
+  //   formatting of the code
+  div.setAttribute("class", "row");
+  p.setAttribute("class", "col-sm-9");
+  btn.setAttribute("class", "btn btn-info col-sm-3");
+
+  btn.onclick = (event) => {
+    music();
+    createCompleted(userInput);
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+  };
+  input.textContent = "";
+
+  todo.appendChild(div);
+};
+
+function music() {
+  sound.play();
+}
+
+function createCompleted(input) {
+  var p = document.createElement("p");
+  var btn = document.createElement("btn");
+  var div = document.createElement("div");
+  p.textContent = input;
+  btn.textContent = "Delete";
+
+  div.appendChild(p);
+  div.appendChild(btn);
+  div.setAttribute("class", "row");
+  p.setAttribute("class", "col-sm-9");
+  btn.setAttribute("class", "btn btn-danger col-sm-3");
+  btn.onclick = (event) => {
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+  };
+
+  completed.appendChild(div);
+}
